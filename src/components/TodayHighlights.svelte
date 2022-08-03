92ee6d4d01fdac86dd96c06e48ac259d490ee169
laindomJS <adrijar21@gmail.com>
2022-08-02 23:38:33 -0400

    corrected colors and favicon

--- src/components/TodayHighlights.svelte
+++ src/components/TodayHighlights.svelte
@@ -15,8 +15,8 @@
             <article class="today-card">
                 <p class="today-title">Humidity</p>
                 <span class="today-data">{highlight.humidity}%</span>
-                <div class="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700 mt-4">
-                    <div class="bg-yellow-400 h-2.5 rounded-full" style="width:{`${highlight.humidity.toString()}%`}"></div>
+                <div class="w-full bg-gray-300 rounded-full h-2.5 dark:bg-gray-700 mt-4">
+                    <div class="dark:bg-yellow-400 bg-green-500 h-2.5 rounded-full" style="width:{`${highlight.humidity.toString()}%`}"></div>
                 </div>
             </article>
 
